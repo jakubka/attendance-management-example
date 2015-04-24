51e05cc00b54a5ca97960393a14134477fe573e6
Jakub Kadlubiec <jakub.kadlubiec@gmail.com>
2015-04-24 11:00:11 +0200

    Forgotten entity

---
+++ AM.Data/Entities/Pass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Data.Entities
+{
+    public enum PassTypeEnum
+    {
+        Arrive,
+        Leave
+    }
+
+    public class Pass
+    {
+        public int Id { get; set; }
+
+        public DateTime Time { get; set;}
+
+        public PassTypeEnum Type { get; set; }
+
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
